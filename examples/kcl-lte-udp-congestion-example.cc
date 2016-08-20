9057948703a3285da498291b82341d53c1eb7b2f
matthias kr <matthias.kreileder@web.de>
2016-08-20 16:53:49 +0100

    Haptic Apps and Udp Echo Apps in an lte network

--- examples/kcl-lte-udp-congestion-example.cc
+++ examples/kcl-lte-udp-congestion-example.cc
@@ -48,6 +48,8 @@ void assignBearer(Ptr<NetDevice> netDev, Ptr<LteHelper> lteHelper){
 	lteHelper->ActivateDedicatedEpsBearer (netDev, bearer, EpcTft::Default ());
 }
 
+void InstallMobilityModel(NodeContainer& ueNodes, NodeContainer& enb, double distance);
+
 /**
  * Sample simulation script for LTE+EPC. It instantiates several eNodeB,
  * attaches one UE per eNodeB starts a flow for each UE to  and from a remote host.
@@ -59,25 +61,21 @@ NS_LOG_COMPONENT_DEFINE ("KclLteCongestionExample");
 int
 main (int argc, char *argv[])
 {
-
-  uint16_t numberOfNodes = 2;
-  double simTime = 31.1;
+  double simTime = 30.0;
+  double appStartTime = 2.0;
   double distance = 60.0;
-  double interPacketInterval = 100;
   std::string protocol = "UDP";
 
   // Command line arguments
   CommandLine cmd;
-  cmd.AddValue("numberOfNodes", "Number of eNodeBs + UE pairs", numberOfNodes);
   cmd.AddValue("simTime", "Total duration of the simulation [s])", simTime);
   cmd.AddValue("distance", "Distance between eNBs [m]", distance);
-  cmd.AddValue("interPacketInterval", "Inter packet interval [ms])", interPacketInterval);
   cmd.AddValue("protocol","The transport layer protocol for the haptic applications: \"UDP\" or \"TCP\" ",protocol);
   cmd.Parse(argc, argv);
 
   ////////////////////////////////////////////////////////////////////////////////
   //
-  //	Radio Access Network and Packet Core
+  //	Prepare Radio Access Network and Packet Core
   //
   ////////////////////////////////////////////////////////////////////////////////
 
@@ -101,6 +99,12 @@ main (int argc, char *argv[])
   Ptr<Node> udpEchoRemoteHost = remoteHostContainer.Get (1);
   InternetStackHelper internet;
   internet.Install (remoteHostContainer);
+
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  //	Prepare Internet and routes
+  //
+  ////////////////////////////////////////////////////////////////////////////////
 
   // Create the connection to the network where the HapticTeleOperator lives
   PointToPointHelper p2ph;
@@ -127,14 +131,11 @@ main (int argc, char *argv[])
   Ptr<Ipv4StaticRouting> udpEchoRemoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (udpEchoRemoteHost->GetObject<Ipv4> ());
   udpEchoRemoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
 
-
-
-
-
-
-
-
-
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  //	Connect the mobile nodes to LTE and set routes to remote nodes
+  //
+  ////////////////////////////////////////////////////////////////////////////////
 
 
   NodeContainer ueNodes;
@@ -145,32 +146,7 @@ main (int argc, char *argv[])
   Ptr<Node> hapticOperatorNode = ueNodes.Get(0);
   Ptr<Node> udpEchoClientNode = ueNodes.Get(1);
 
-  // Install Mobility Model
-  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-  for (uint16_t i = 0; i < 2; i++)
-    {
-      positionAlloc->Add (Vector(distance * i, 0, 0));
-    }
-  MobilityHelper mobility;
-  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  mobility.SetPositionAllocator(positionAlloc);
-  mobility.Install(ueNodes);
-
-
-  // Install Mobility Model
-  Ptr<ListPositionAllocator> enbPositionAlloc = CreateObject<ListPositionAllocator> ();
-  enbPositionAlloc->Add (Vector(distance * 0.5, 0, 0));
-  mobility.SetPositionAllocator(enbPositionAlloc);
-  mobility.Install(enbNode);
-
-
-
-
-
-
-
-
-
+  InstallMobilityModel(ueNodes,enbNode, distance);
 
 
   // Install LTE Devices to the nodes
@@ -206,8 +182,12 @@ main (int argc, char *argv[])
   //
   //////////////////////////////////////////////////////////////////////////////////////
 
+  // constructing the file name used by the flow-monitor
   std::stringstream ss;
   ss << "kcl-lte";
+  //
+  //	UDP
+  //
   if (protocol.compare("UDP") == 0){
 
 	  NS_LOG_DEBUG("Installing Udp Haptic Applications");
@@ -215,22 +195,31 @@ main (int argc, char *argv[])
 
 	  int hapticPort = 4444;
 	  HapticTeleOperatorHelper teleOperator (hapticPort);
-	  teleOperator.SetAttribute("FileName", StringValue ("src/Kcl-Haptic-Sim/test/force.txt"));
+	  teleOperator.SetAttribute("FileName", StringValue ("src/Kcl-Haptic-Sim/test-data/force.txt"));
 	  teleOperator.SetAttribute ("SamplingIntervalSeconds", DoubleValue (0.001));
+	  /*
+	   * Un-comment if you want to apply data reduction
+	   */
 	  //teleOperator.SetAttribute ("ApplyDataReduction", BooleanValue (true));
 	  ApplicationContainer apps = teleOperator.Install (remoteHost);
-	  apps.Start (Seconds (0.01));
-	  apps.Stop (Seconds (30.0));
+	  apps.Start (Seconds (appStartTime - 0.5));	// Let's start the HapticTeleOperator slightly before the HapticOperator
+	  apps.Stop (Seconds (simTime));
 
 	  HapticOperatorHelper client (remoteHostAddr, hapticPort);
-	  client.SetAttribute ("PositionFile", StringValue ("src/Kcl-Haptic-Sim/test/position.txt"));
-	  client.SetAttribute ("VelocityFile", StringValue ("src/Kcl-Haptic-Sim/test/fakeVelocity.txt"));
+	  client.SetAttribute ("PositionFile", StringValue ("src/Kcl-Haptic-Sim/test-data/position.txt"));
+	  client.SetAttribute ("VelocityFile", StringValue ("src/Kcl-Haptic-Sim/test-data/velocity.txt"));
 	  client.SetAttribute ("SamplingIntervalSeconds", DoubleValue (0.001));
-	  //client.SetAttribute ("FileType", StringValue ("POSITION"));
+	  /*
+	   * Un-comment if you want to apply data reduction
+	   */
+	  //client.SetAttribute ("ApplyDataReduction", BooleanValue (true));
 	  apps = client.Install (ueNodes.Get(0));
-	  apps.Start (Seconds (2.0));
-	  apps.Stop (Seconds (30.0));
+	  apps.Start (Seconds (appStartTime));
+	  apps.Stop (Seconds (simTime));
   }
+  //
+  //	TCP
+  //
   else if (protocol.compare("TCP") == 0){
 	  ss << "-tcp";
 	  NS_LOG_DEBUG("Installing Tcp Haptic Applications");
@@ -243,8 +232,8 @@ main (int argc, char *argv[])
 	   */
 	  //tcpTeleHelper.SetAttribute ("ApplyDataReduction", BooleanValue (true));
 	  ApplicationContainer tcpTeleApps = tcpTeleHelper.Install(remoteHost);
-	  tcpTeleApps.Start (Seconds(0));
-	  tcpTeleApps.Stop (Seconds (20.));
+	  tcpTeleApps.Start (Seconds(appStartTime - 0.5));
+	  tcpTeleApps.Stop (Seconds (simTime));
 
 	  TcpHapticOperatorHelper tcpHOP (remoteHostAddr,hapticPort);
 	  tcpHOP.SetAttribute ("PositionFile", StringValue ("src/Kcl-Haptic-Sim/test/position.txt"));
@@ -255,14 +244,13 @@ main (int argc, char *argv[])
 	   */
 	  //tcpHOP.SetAttribute ("ApplyDataReduction", BooleanValue (true));
 	  ApplicationContainer apps = tcpHOP.Install (ueNodes.Get(0));
-	  apps.Start (Seconds (2.0));
-	  apps.Stop (Seconds (20.0));
+	  apps.Start (Seconds (appStartTime));
+	  apps.Stop (Seconds (simTime));
   }
   else {
-	  ss << "-not-supported";
 	  NS_LOG_DEBUG("Currently only UDP and TCP are supported for Haptic Applications.");
+	  exit(0);
   }
-  //lteHelper->EnableTraces ();
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
   //
@@ -276,8 +264,8 @@ main (int argc, char *argv[])
     uint16_t udpEchport = 9;  // well-known echo port number
     UdpEchoServerHelper server (udpEchport);
     ApplicationContainer apps = server.Install (udpEchoRemoteHost);
-    apps.Start (Seconds (1.0));
-    apps.Stop (Seconds (10.0));
+    apps.Start (Seconds (appStartTime - 0.5));
+    apps.Stop (Seconds (simTime));
 
   //
   // Create a UdpEchoClient application to send UDP datagrams from node zero to
@@ -291,8 +279,8 @@ main (int argc, char *argv[])
     client.SetAttribute ("Interval", TimeValue (interPacketIntervalUdpEcho));
     client.SetAttribute ("PacketSize", UintegerValue (packetSize));
     apps = client.Install (udpEchoClientNode);
-    apps.Start (Seconds (2.0));
-    apps.Stop (Seconds (10.0));
+    apps.Start (Seconds (appStartTime));
+    apps.Stop (Seconds (simTime));
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
   //
@@ -308,22 +296,34 @@ main (int argc, char *argv[])
   allNodes.Add(ueNodes);
   flowMonitor = flowHelper.Install(allNodes);
 
-  // Uncomment to enable PCAP tracing
-  //p2ph.EnablePcapAll("lena-epc-first");
-
   Simulator::Stop(Seconds(simTime));
   Simulator::Run();
 
   ss << "-example.xml";
   flowMonitor->SerializeToXmlFile(ss.str(), true, true);
 
-  /*GtkConfigStore config;
-  config.ConfigureAttributes();*/
-
   Simulator::Destroy();
   return 0;
 
 }
 
+void InstallMobilityModel(NodeContainer& ueNodes, NodeContainer& enb, double distance){
+
+	  // Install Mobility Model
+	  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+	  for (uint16_t i = 0; i < 2; i++)
+	    {
+	      positionAlloc->Add (Vector(distance * i, 0, 0));
+	    }
+	  MobilityHelper mobility;
+	  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+	  mobility.SetPositionAllocator(positionAlloc);
+	  mobility.Install(ueNodes);
 
 
+	  // Install Mobility Model
+	  Ptr<ListPositionAllocator> enbPositionAlloc = CreateObject<ListPositionAllocator> ();
+	  enbPositionAlloc->Add (Vector(distance * 0.5, 0, 0));
+	  mobility.SetPositionAllocator(enbPositionAlloc);
+	  mobility.Install(enb);
+}
