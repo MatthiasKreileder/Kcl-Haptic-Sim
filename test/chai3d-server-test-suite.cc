775210f0812be32418284664b46de0393f66098e
MatthiasKreileder <matthias.kreileder@kcl.ac.uk>
2016-08-24 19:23:42 +0100

    Update chai3d-server-test-suite.cc

--- test/chai3d-server-test-suite.cc
+++ test/chai3d-server-test-suite.cc
@@ -15,6 +15,9 @@
 #include "ns3/point-to-point-module.h"
 #include <string>
 
+// Idea and code to create the ethernet network is based on:
+// https://www2.nsnam.org/doxygen/second_8py_source.html
+
 // Network topology
 //
 //       n0    n1   n2   n3
