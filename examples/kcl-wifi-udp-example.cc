5c44c32c6bd4e14c2bfa384f1c2483dafe5a05d2
MatthiasKreileder <matthias.kreileder@kcl.ac.uk>
2016-08-24 19:21:34 +0100

    Adding citation

--- examples/kcl-wifi-udp-example.cc
+++ examples/kcl-wifi-udp-example.cc
@@ -14,6 +14,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+// Idea and code to create the wifi + ethernet network is based on:
+// https://www2.nsnam.org/doxygen/third_8cc_source.html
+
 #include "ns3/core-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/network-module.h"
