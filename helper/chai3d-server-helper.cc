e1162c74d29ba3f7ef32956ee8874c663dbaa350
MatthiasKreileder <matthias.kreileder@kcl.ac.uk>
2016-08-25 21:19:43 +0100

    rm log

--- helper/chai3d-server-helper.cc
+++ helper/chai3d-server-helper.cc
@@ -1,9 +1,4 @@
-/*
- * haptic-operator-helper.cc
- *
- *  Created on: 13 Jul 2016
- *      Author: matthias
- */
+
 
 //
 // Code and idea based on: https://www.nsnam.org/doxygen/udp-echo-helper_8cc_source.html
