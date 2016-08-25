638674e526c02843b33253a3329340d64feb1962
MatthiasKreileder <matthias.kreileder@kcl.ac.uk>
2016-08-25 21:15:17 +0100

    Adding citations

--- model/tcp-haptic-operator.h
+++ model/tcp-haptic-operator.h
@@ -4,7 +4,9 @@
  *  Created on: 17 Aug 2016
  *      Author: matthias
  */
-
+//
+//	Code and idea based on: https://www.nsnam.org/doxygen/udp-client_8cc_source.html
+//
 #ifndef SRC_KCL_HAPTIC_SIM_MODEL_TCP_HAPTIC_OPERATOR_H_
 #define SRC_KCL_HAPTIC_SIM_MODEL_TCP_HAPTIC_OPERATOR_H_
 
