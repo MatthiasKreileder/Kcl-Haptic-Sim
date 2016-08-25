297bab6bf0435211997d0a35747e8bc95d0ef131
MatthiasKreileder <matthias.kreileder@kcl.ac.uk>
2016-08-25 21:20:39 +0100

    rm log

--- helper/haptic-tele-operator-helper.h
+++ helper/haptic-tele-operator-helper.h
@@ -1,9 +1,4 @@
-/*
- * haptic-tele-operator-helper.h
- *
- *  Created on: 19 Jul 2016
- *      Author: matthias
- */
+
 //
 // Code and idea based on: https://www.nsnam.org/doxygen/udp-echo-helper_8cc_source.html
 //
