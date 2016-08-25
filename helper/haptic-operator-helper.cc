0a51d88851d611d36f49a7b7e59e049b89f1a3b1
MatthiasKreileder <matthias.kreileder@kcl.ac.uk>
2016-08-25 21:05:22 +0100

    Adding citations

--- helper/haptic-operator-helper.cc
+++ helper/haptic-operator-helper.cc
@@ -4,7 +4,9 @@
  *  Created on: 13 Jul 2016
  *      Author: matthias
  */
-
+//
+// Code and idea based on: https://www.nsnam.org/doxygen/udp-echo-helper_8cc_source.html
+//
 #include "haptic-operator-helper.h"
 
 #include "haptic-operator-helper.h"
